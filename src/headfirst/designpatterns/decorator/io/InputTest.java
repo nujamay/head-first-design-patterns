643cefec24364bd2b5d31992f684c763ef6f285b
nujamay <junya0220yamashita@gmail.com>
2021-01-05 14:48:17 +0900

    add a comment

--- src/headfirst/designpatterns/decorator/io/InputTest.java
+++ src/headfirst/designpatterns/decorator/io/InputTest.java
@@ -13,8 +13,15 @@ public class InputTest {
   public static void main(String[] args) {
     int c;
     try {
-      InputStream in = new LowerCaseInputStream(
-          new BufferedInputStream(new FileInputStream("resources/test.txt")));
+      /*
+       * 1. Create a `FileInputStream` by opening a connection to an actual file.
+       * 2. Decorate the stream with `BufferedInputStream`.
+       * 3. Decorate the stream with `LowerCaseInputStream` declared by me.
+       */
+      InputStream in =
+          new LowerCaseInputStream(
+              new BufferedInputStream(
+                  new FileInputStream("resources/test.txt")));
 
       while ((c = in.read()) >= 0) {    // read and convert up to end of the stream.
         System.out.print((char) c);
